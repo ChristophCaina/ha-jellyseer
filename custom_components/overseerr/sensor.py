6ea77672bb368664f91e8b757548115f5f8fc673
Christoph <Christoph@Caina.de>
2024-07-19 17:43:51 +0200

    Update sensor.py

--- custom_components/overseerr/sensor.py
+++ custom_components/overseerr/sensor.py
@@ -1,8 +1,8 @@
-"""Support for Overseerr."""
+"""Support for Jellyseer."""
 from datetime import timedelta
 import logging
 
-from pyoverseerr import OverseerrError
+from jellyseerrpy import JellyseerError
 
 from homeassistant.helpers.entity import Entity
 
@@ -14,40 +14,40 @@ SCAN_INTERVAL = timedelta(seconds=86400)
 
 
 def setup_platform(hass, config, add_entities, discovery_info=None):
-    """Set up the Overseerr sensor platform."""
+    """Set up the Jellyseerr sensor platform."""
     if discovery_info is None:
         return
 
     sensors = []
 
-    overseerr = hass.data[DOMAIN]["instance"]
+    jellyseerr = hass.data[DOMAIN]["instance"]
 
     for sensor in SENSOR_TYPES:
         sensor_label = sensor
         sensor_type = SENSOR_TYPES[sensor]["type"]
         sensor_icon = SENSOR_TYPES[sensor]["icon"]
-        sensors.append(OverseerrSensor(
-            sensor_label, sensor_type, overseerr, sensor_icon))
+        sensors.append(JellyseerrSensor(
+            sensor_label, sensor_type, jellyseerr, sensor_icon))
 
     add_entities(sensors, True)
 
 
 class OverseerrSensor(Entity):
-    """Representation of an Overseerr sensor."""
+    """Representation of an Jellyseerr sensor."""
 
-    def __init__(self, label, sensor_type, overseerr, icon):
+    def __init__(self, label, sensor_type, jellyseerr, icon):
         """Initialize the sensor."""
         self._state = None
         self._label = label
         self._type = sensor_type
-        self._overseerr = overseerr
+        self._jellyseerr = jellyseerr
         self._icon = icon
         self._last_request = None
 
     @property
     def name(self):
         """Return the name of the sensor."""
-        return f"Overseerr {self._type}"
+        return f"Jellyseerr {self._type}"
 
     @property
     def icon(self):
@@ -66,11 +66,11 @@ class OverseerrSensor(Entity):
 
     def update(self):
         """Update the sensor."""
-        _LOGGER.debug("Update Overseerr sensor: %s", self.name)
+        _LOGGER.debug("Update Jellyseerr sensor: %s", self.name)
         try:
             if self._label == "issues":
-                issueCounts = self._overseerr.issueCounts
-                lastIssue = self._overseerr.last_issue
+                issueCounts = self._jellyseerr.issueCounts
+                lastIssue = self._jellyseerr.last_issue
                 self._state = issueCounts["open"]
                 merged_dict = issueCounts
                 if (lastIssue is not None):
@@ -79,24 +79,24 @@ class OverseerrSensor(Entity):
                 self._last_request = merged_dict
 
             if self._label == "movies":
-                self._state = self._overseerr.movie_requests
-                self._last_request = self._overseerr.last_movie_request
+                self._state = self._jellyseerr.movie_requests
+                self._last_request = self._jellyseerr.last_movie_request
             elif self._label == "total":
-                self._state = self._overseerr.total_requests
-                self._last_request = self._overseerr.last_total_request
+                self._state = self._jellyseerr.total_requests
+                self._last_request = self._jellyseerr.last_total_request
             elif self._label == "tv":
-                self._state = self._overseerr.tv_requests
-                self._last_request = self._overseerr.last_tv_request
+                self._state = self._jellyseerr.tv_requests
+                self._last_request = self._jellyseerr.last_tv_request
             elif self._label == "music":
-                self._state = self._overseerr.music_requests
+                self._state = self._jellyseerr.music_requests
                 self._last_request = "Not Supported"
             elif self._label == "pending":
-                self._state = self._overseerr.pending_requests
-                self._last_request = self._overseerr.last_pending_request
+                self._state = self._jellyseerr.pending_requests
+                self._last_request = self._jellyseerr.last_pending_request
             elif self._label == "approved":
-                self._state = self._overseerr.approved_requests
+                self._state = self._jellyseerr.approved_requests
             elif self._label == "available":
-                self._state = self._overseerr.available_requests
+                self._state = self._jellyseerr.available_requests
         except OverseerrError as err:
-            _LOGGER.warning("Unable to update Overseerr sensor: %s", err)
+            _LOGGER.warning("Unable to update Jellyseerr sensor: %s", err)
             self._state = None
